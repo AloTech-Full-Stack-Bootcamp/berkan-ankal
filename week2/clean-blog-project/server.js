e7d1511a4681c3d2a232278755b12f2c33f263a4
brknkl <berkanankal68@gmail.com>
2021-11-05 18:10:29 +0300

    post-detail-page-completed

--- week2/clean-blog-project/server.js
+++ week2/clean-blog-project/server.js
@@ -5,10 +5,13 @@ const Post = require("./models/Post");
 const app = express();
 const PORT = 5010;
 
+// Connect DB
 mongoose.connect("mongodb://localhost/cleanblog-test-db");
 
+// Template Engine
 app.set("view engine", "ejs");
 
+// Middlewares
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -33,6 +36,12 @@ app.get("/add", (req, res) => {
 app.post("/posts", async (req, res) => {
   await Post.create(req.body);
   res.redirect("/");
+});
+
+app.get("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  res.render("post", { post });
 });
 
 app.listen(PORT, () =>
