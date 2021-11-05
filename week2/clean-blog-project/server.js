595d089062720d08843976bdfd033c1a3325ad30
brknkl <berkanankal68@gmail.com>
2021-11-05 17:23:39 +0300

    add-post completed

--- week2/clean-blog-project/server.js
+++ week2/clean-blog-project/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
+const Post = require("./models/Post");
 
 const app = express();
 const PORT = 5010;
 
+mongoose.connect("mongodb://localhost/cleanblog-test-db");
+
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", { posts });
 });
 
 app.get("/post", (req, res) => {
@@ -22,6 +28,11 @@ app.get("/about", (req, res) => {
 
 app.get("/add", (req, res) => {
   res.render("add_post");
+});
+
+app.post("/posts", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
 });
 
 app.listen(PORT, () =>
